0ed86ccbb5477838e4b90100119ddd08356c8a46
mohd-ashif <mohdaszif077@gmail.com>
2024-05-09 21:19:51 +0530

    update

--- index.js
+++ index.js
@@ -6,24 +6,19 @@ import seedRouter from './routes/seedRoute.js';
 import productRouter from './routes/productRoute.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoute.js';
-import  path from 'path'
+import path from 'path';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URI, {})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
-
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Enable CORS globally 
 
+// Enable CORS globally
 app.use(cors({
   origin: function (origin, callback) {
-   
     if (!origin || origin === process.env.FRONTEND_URL) {
       callback(null, true);
     } else {
@@ -34,12 +29,16 @@ app.use(cors({
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
 }));
 
-
-app.get('/', (req, res) => {
-  res.send('Server is running...')
-})
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
+
 app.get('/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
@@ -49,15 +48,16 @@ app.use('/products', productRouter);
 app.use('/users', userRouter);
 app.use('/orders', orderRouter);
 
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
+// Serve static files from the '/uploads' directory
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+// Start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
