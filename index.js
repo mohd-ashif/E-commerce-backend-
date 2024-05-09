a9b869ed50e6175307c5d552afeb4eb2f0469812
mohd-ashif <mohdaszif077@gmail.com>
2024-05-09 21:10:28 +0530

    update

--- index.js
+++ index.js
@@ -6,70 +6,60 @@ import seedRouter from './routes/seedRoute.js';
 import productRouter from './routes/productRoute.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoute.js';
-import  path from 'path'
+import path from 'path';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URI, {})
+const app = express();
+const __dirname = path.resolve();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
-const app = express();
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Enable CORS globally 
 
+// Enable CORS
+const allowedOrigins = [process.env.FRONTEND_URL ]; // Add more origins as needed
 app.use(cors({
   origin: function (origin, callback) {
-   
-    if (!origin || origin === process.env.FRONTEND_URL) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
 }));
 
-
-// app.get('/', (req, res) => {
-//   res.send('Server is running...')
-// })
+// Serve static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-app.get('/keys/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});
-
 app.use('/seed', seedRouter);
 app.use('/products', productRouter);
 app.use('/users', userRouter);
 app.use('/orders', orderRouter);
 
-
-const __dirname = path.resolve(); 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-
+// Serve React frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
-
 } else {
-  app.get('/',(req, res)=>{
-      res.send('Api is running...')
+  app.get('/', (req, res) => {
+    res.send('API is running...');
   });
 }
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+  console.error(err.stack);
+  res.status(500).send({ message: 'Something broke!' });
 });
 
 const port = process.env.PORT || 5000;
