d80ed0221edc28d860af75b87ae9153dcf4b66d7
mohd-ashif <mohdaszif077@gmail.com>
2024-05-09 21:02:02 +0530

    update

--- index.js
+++ index.js
@@ -50,19 +50,16 @@ app.use('/users', userRouter);
 app.use('/orders', orderRouter);
 
 
-
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/build/index.html'))
-);
-
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);  
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
